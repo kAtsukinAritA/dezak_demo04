799c8166278d20b04bce15fa82ccf86fd602584a
kAtsukinAritA <64456240+kAtsukinAritA@users.noreply.github.com>
2021-05-25 21:10:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+window.onload = () => {
+  // #image1に画像を描画
+  drawConcat();
+
+  // #image2にテキストを描画
+  drawImage2();
+
+  // 「+」ボタンを押したら合成
+  document.querySelector("#btn-concat").addEventListener("click", ()=>{
+    concatCanvas("#concat", ["#image2"]);
+  });
+
+  // 「消しゴム」ボタンを押したらクリア
+  document.querySelector("#btn-eraser").addEventListener("click", ()=>{
+    eraseCanvas("#concat");
+  });
+
+};
+
+/**
+ * [onload] うな重の画像を描画
+ */
+function drawConcat(){
+  const Nui = new Image();
+  Nui.src = "images/nui.PNG";
+  Nui.onload = () =>{
+    const canvas = document.querySelector("#concat");
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(Nui, 0, 0, canvas.width, canvas.height);
+  }
+}
+
+/**
+ * [onload] 洋服を描画
+ */
+function drawImage2(){
+   const Body1 = new Image();
+  Body1.src = "images/body01.PNG";
+  Body1.onload = () =>{
+    const canvas = document.querySelector("#image2");
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(Body1, 0, 0, canvas.width, canvas.height);
+  }
+}
+
+
+
+
+/**
+ * Canvas合成
+ *
+ * @param {string} base 合成結果を描画するcanvas(id)
+ * @param {array} asset 合成する素材canvas(id)
+ * @return {void}
+ */
+ async function concatCanvas(base, asset){
+  const canvas = document.querySelector(base);
+  const ctx = canvas.getContext("2d");
+
+  for(let i=0; i<asset.length; i++){
+    const image2 = await getImagefromCanvas(asset[i]);
+    ctx.drawImage(image2, 0, 0, canvas.width, canvas.height);
+  }
+}
+
+/**
+ * Canvasをすべて削除する
+ *
+ * @param {string} target 対象canvasのid
+ * @return {void}
+ */
+function eraseCanvas(target){
+  const Nui = new Image();
+  Nui.src = "images/nui.PNG";
+  Nui.onload = () =>{
+    const canvas = document.querySelector("#concat");
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(Nui, 0, 0, canvas.width, canvas.height);
+    }
+
+//   const canvas = document.querySelector(target);
+//   const ctx = canvas.getContext("2d");
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+/**
+ * Canvasを画像として取得
+ *
+ * @param {string} id  対象canvasのid
+ * @return {object}
+ */
+function getImagefromCanvas(id){
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    const ctx = document.querySelector(id).getContext("2d");
+    image.onload = () => resolve(image);
+    image.onerror = (e) => reject(e);
+    image.src = ctx.canvas.toDataURL();
+  });
+}
